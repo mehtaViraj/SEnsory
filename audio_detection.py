519aeaecb900c2e8bb6c6045d08b505fd65da5d9
DhruvBhatia0 <69252327+DhruvBhatia0@users.noreply.github.com>
2021-10-28 14:33:30 -0400

    updated dataset structure

--- audio_detection.py
+++ audio_detection.py
@@ -3,33 +3,28 @@ import keras
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D, LSTM
 from tensorflow.keras.utils import to_categorical
-import wandb
-from wandb.keras import WandbCallback
-import matplotlib.pyplot as plt
 
-wandb.init()
-config = wandb.config
 
-config.max_len = 11
-config.buckets = 20
+max_len = 11
+buckets = 20
 
 # Save data to array file first
-save_data_to_array(max_len=config.max_len, n_mfcc=config.buckets)
+save_data_to_array(max_len=max_len, n_mfcc=buckets)
 
-labels=["audio0","audio1","audio2","audio3","audio4"]
+labels=["audio0","audio1","audio2",]
 
 # # Loading train set and test set
 X_train, X_test, y_train, y_test = get_train_test()
 
 # # Feature dimension
 channels = 1
-config.epochs = 50
-config.batch_size = 100
+epochs = 50
+batch_size = 100
 
-num_classes = 5
+num_classes = len(labels)
 
-X_train = X_train.reshape(X_train.shape[0], config.buckets, config.max_len, channels)
-X_test = X_test.reshape(X_test.shape[0], config.buckets, config.max_len, channels)
+X_train = X_train.reshape(X_train.shape[0], buckets, max_len, channels)
+X_test = X_test.reshape(X_test.shape[0], buckets, max_len, channels)
 
 #plt.imshow(X_train[100, :, :, 0])
 #print(y_train[100])
@@ -37,16 +32,31 @@ X_test = X_test.reshape(X_test.shape[0], config.buckets, config.max_len, channel
 y_train_hot = to_categorical(y_train)
 y_test_hot = to_categorical(y_test)
 
-X_train = X_train.reshape(X_train.shape[0], config.buckets, config.max_len)
-X_test = X_test.reshape(X_test.shape[0], config.buckets, config.max_len)
+X_train = X_train.reshape(X_train.shape[0], buckets, max_len)
+X_test = X_test.reshape(X_test.shape[0], buckets, max_len)
 
 model = Sequential()
-model.add(Flatten(input_shape=(config.buckets, config.max_len)))
+model.add(Flatten(input_shape=(buckets, max_len)))
 model.add(Dense(num_classes, activation='softmax'))
+#model.add(Dropout(0.1))
 model.compile(loss="categorical_crossentropy",
                   optimizer="adam",
                   metrics=['accuracy'])
 
-wandb.init()
-model.fit(X_train, y_train_hot, epochs=config.epochs, validation_data=(X_test, y_test_hot), callbacks=[WandbCallback(data_type="image", labels=labels)])
+'''model = Sequential()
+model.add(Conv2D(32,
+    (3,3),
+    input_shape=(buckets, max_len),
+    activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dense(num_classes, activation='softmax'))
+
+model.compile(loss="categorical_crossentropy",
+                  optimizer="adam",
+                  metrics=['accuracy'])'''
+
+
+model.fit(X_train, y_train_hot, epochs=50, validation_data=(X_test, y_test_hot))
 model.summary()
